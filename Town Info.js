94dac9cca8ee8194032171ecdb003658247a89f0
Evgenia1819 <95176074+Evgenia1819@users.noreply.github.com>
2021-12-08 01:04:42 +0000

    Add files via upload

---
+++ Town Info.js
@@ -0,0 +1,8 @@
+function townInfo(town, population, area) {
+  let result = `Town ${town} has population of ${population} and area ${area} squire km.`;
+  console.log(result);
+}
+
+
+townInfo('Sofia', 1286383, 492);
+
